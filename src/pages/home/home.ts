112465234e300e54a335cc4166b181d14f987dea
Tex08 <alex@pelletier.com>
2017-12-14 15:31:32 -0500

    #1 Removed unused import

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { SettingsPage } from '../settings/settings';
 import { GamePage } from '../game/game';
 
 @Component({
