a99f1304c58b663de77513b0dcd37076b6f523c5
Tex08 <alex@pelletier.com>
2017-12-14 17:03:10 -0500

    #2 Added logic to input Xs and Os on the game grid.

--- src/pages/game/game.ts
+++ src/pages/game/game.ts
@@ -7,8 +7,19 @@ import { NavController } from 'ionic-angular';
 })
 export class GamePage {
 
+	squares = [];
+	player = "O";
+
   constructor(public navCtrl: NavController) {
 
+  }
+
+  handleMove(position) : void {
+  	if (this.squares[position] !== "X" && this.squares[position] !== "O") {
+  		this.player = this.player === "X" ? "O" : "X";
+  		this.squares[position] = this.player;
+  	}
+  	//this.squares[position] = "O";
   }
 
 }
