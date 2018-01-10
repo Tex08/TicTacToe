6632078555d564b0d26a14a634d76d47b1bc4899
Tex08 <alex@pelletier.com>
2018-01-10 18:51:52 -0500

    #11 Changed the way message appears.

--- src/pages/game/game.ts
+++ src/pages/game/game.ts
@@ -10,15 +10,17 @@ export class GamePage {
 	squares = [];
 	player = "O";
 	display = "X";
+  message = "";
 
   constructor(public navCtrl: NavController) {
-
+    this.message = "It is currently "+this.display+"'s turn";
   }
 
   handleMove(position) : void {
   	if (this.squares[position] !== "X" && this.squares[position] !== "O") {
   		this.player = this.player === "O" ? "X" : "O";
   		this.display = this.display === "X" ? "O" : "X";
+      this.message = "It is currently "+this.display+"'s turn";
   		this.squares[position] = this.player;
   	}
   	//this.squares[position] = "O";
