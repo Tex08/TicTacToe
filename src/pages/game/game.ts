1b483bb4081f4320041de58747f8f4ffbc638d9f
Tex08 <alex@pelletier.com>
2018-01-10 19:00:56 -0500

    #11 Added a win check, and associated message change and game disable when a win occurs.

--- src/pages/game/game.ts
+++ src/pages/game/game.ts
@@ -11,17 +11,22 @@ export class GamePage {
 	player = "O";
 	display = "X";
   message = "";
+  win = false;
 
   constructor(public navCtrl: NavController) {
     this.message = "It is currently "+this.display+"'s turn";
   }
 
   handleMove(position) : void {
-  	if (this.squares[position] !== "X" && this.squares[position] !== "O") {
+  	if (this.squares[position] !== "X" && this.squares[position] !== "O" && this.win !== true) {
   		this.player = this.player === "O" ? "X" : "O";
   		this.display = this.display === "X" ? "O" : "X";
       this.message = "It is currently "+this.display+"'s turn";
   		this.squares[position] = this.player;
+      if (this.checkWin()) {
+        this.win = true;
+        this.message = this.player+" has made a Tic Tac Toe! \n (Press the reset button to play again)";
+      }
   	}
   	//this.squares[position] = "O";
   }
@@ -30,6 +35,12 @@ export class GamePage {
   	for(var i=0;i<this.squares.length;i++) {
   		this.squares[i] = "";
   	}
+    this.message = "It is currently "+this.display+"'s turn";
+    this.win = false;
+  }
+
+  checkWin() : boolean {
+    return true;
   }
 
 }
